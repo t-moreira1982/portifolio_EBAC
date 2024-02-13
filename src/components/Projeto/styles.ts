93c0e559f9d4e62617a8ff9c852d789034cb0b9f
Thiago <thiago_moreira22@hotmail.com>
2024-02-13 13:50:02 -0300

    Criando a seção de projetos e o componente de projetos

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  border: 1px solid #c1c1c1;
+  padding: 16px;
+`
+
+export const LinkBotao = styled.a`
+  color: #fff;
+  font-size: 14px;
+  background-color: #4476bf;
+  text-decoration: none;
+  padding: 8px;
+  display: inline-block;
+  margi-top: 24px;
+`
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import Titulo from '../../components/Titulo'
+import Projeto from '../../components/Projeto'
+
+import { Lista } from './styles'
 
 const Projetos = () => (
   <section>
     <Titulo fontSize={16}>Projetos</Titulo>
+    <Lista>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+    </Lista>
   </section>
 )
 
